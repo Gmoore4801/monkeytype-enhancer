66f28fe1d28bcedfa737ce53a1e2e3decf584d12
Gmoore4801 <gavin00moore@gmail.com>
2024-07-30 17:33:10 -0400

    Updated the popup and timing of the mode change

--- contentScript.js
+++ contentScript.js
@@ -26,13 +26,13 @@ window.addEventListener('keypress', e => {
     const delay = ms => new Promise(res => setTimeout(res, ms));
 
     async function setCustomMode(language) {
-        let letter;
+        let letter = "a";
         let max = 0;
         console.log("Your accuracy for each letter:");
         for (let i = 0; i < 26; i++) {
             let l = String.fromCharCode(97 + i);
-            let c = correct.get(l) == null ? 0 : correct.get(l); // number of this letter correct
-            let w = incorrect.get(l) == null ? 0 : incorrect.get(l); // number of this letter wrong
+            let c = correct.get(l) == null ? 0 : correct.get(l);
+            let w = incorrect.get(l) == null ? 0 : incorrect.get(l);
             let percent = c + w == 0 ? null : Math.round(100 * c / (c + w));
             let total = c + w;
             console.log(l + ": " + percent + "%    " + c + "/" + total);
@@ -50,12 +50,12 @@ window.addEventListener('keypress', e => {
         document.querySelector('.wordIncludeInput').value = letter;
         document.querySelector('.wordExcludeInput').value = '-';
         document.querySelector('.setButton').click();
-        await delay(500);
+        await delay(150);
         document.querySelector('.buttonGroup [value="random"]').click();
         document.querySelector('.textarea').remove();
         document.querySelector('.buttonsTop').remove();
         document.querySelector('.challengeWarning').insertAdjacentHTML("afterend", `<h2>Select the test length (15 is recommended)</h2>`);
-        await delay(500);
+        
         document.querySelector('.group[data-id="fancy"]').remove();
         document.querySelector('.group[data-id="control"]').remove();
         document.querySelector('.group[data-id="delimiter"]').remove();
@@ -72,6 +72,7 @@ window.addEventListener('keypress', e => {
         document.querySelector('#customTextModal .modal').style.width = '400px';
         document.querySelector('.time').placeholder += " (seconds)";
         document.querySelector('.inputs').style.gridTemplateColumns = '1fr';
+        await delay(100);
         document.querySelector('.time').select();
     }
     
